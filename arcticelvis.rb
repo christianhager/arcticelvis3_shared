1ad68bdec71fae72586b3492010967159bf7ff0c
Christian Hager <persson.hager@gmail.com>
2012-01-23 16:15:20 +0100

    reorganize

---
+++ arcticelvis.rb
@@ -0,0 +1,6 @@
+require 'lib/sending_extention'
+require 'lib/link_extention'
+
+module ArcticElvis
+
+end
--- arcticelvis3_shared.rb
+++
@@ -1,7 +0,0 @@
-require 'extentions/sending_extention'
-require 'extentions/link_extention'
-
-module ArcticElvis
-  module Shared
-  end
-end
