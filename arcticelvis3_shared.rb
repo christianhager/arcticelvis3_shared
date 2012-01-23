259cf87562be89f45b05a933572df688e1016f7a
Christian Hager <persson.hager@gmail.com>
2012-01-23 16:02:01 +0100

    reorganize

---
+++ arcticelvis3_shared.rb
@@ -0,0 +1,7 @@
+require 'extentions/sending_extention'
+require 'extentions/link_extention'
+
+module ArcticElvis
+  module Shared
+  end
+end
---
+++ extentions/link_extention.rb
@@ -0,0 +1,12 @@
+module ArcticElvis
+  module Shared
+    module LinkExtention
+      extend ActiveSupport::Concern
+      included do
+        field :click_count, :type => Integer, :default => 0
+        field :url, :type => String
+        embedded_in :sending
+      end
+    end
+  end
+end
